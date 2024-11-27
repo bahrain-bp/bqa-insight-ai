1a809554f706ecf409cbb2ce450fdc4976391cbf
Ali Jasim <ali.jasim.1203@gmail.com>
2024-11-27 13:13:19 +0300

    updated config

--- sst.config.ts
+++ sst.config.ts
@@ -15,19 +15,19 @@ import { BedrockStack } from "./stacks/BedrockStack";
 export default {
   config(_input) {
     // Restrict stages to dev or prod
-    const allowedStages = ["dev", "devops-coca","devops-gh","prod"];
-    if (!allowedStages.includes(_input.stage ?? "")) {
-      throw new Error(
-          `Invalid stage: ${_input.stage}. Allowed stages are ${allowedStages.join(", ")}.
-          \n To deploy to a new stage, use "sst deploy --stage dev"`
-      );
-    }
     return {
       name: "insight-ai",
       region: "us-east-1",
     };
   },
   stacks(app) {
+    const allowedStages = ["dev", "devops-coca","devops-gh","prod"];
+    if (!allowedStages.includes(app.stage ?? "")) {
+      throw new Error(
+          `Invalid stage: ${app.stage}. Allowed stages are ${allowedStages.join(", ")}.
+          \n To deploy to a new stage, use "npx sst deploy --stage dev"`
+      );
+    }
     // Remove all resources when non-prod stages are removed
     if (app.stage !== "prod") {
       app.setDefaultRemovalPolicy("destroy");
