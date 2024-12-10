511a2c267fca891cfb6d86705b5ba4ee03a6c2d9
theMycron <alyasfy12@gmail.com>
2024-12-10 21:56:22 +0300

    fix slots

--- packages/functions/src/LexBot/intentAmazonLexFulfillment.py
+++ packages/functions/src/LexBot/intentAmazonLexFulfillment.py
@@ -7,10 +7,10 @@ def dispatch(event):
 
     intent_name = event.get('sessionState', {}).get('intent', {}).get('name')
 
-    if intent_name == 'WelcomeIntent':
+    if intent_name == 'BQAIntent':
         bqa_slot = event.get('sessionState', {}).get('intent', {}).get('slots', {}).get('BQASlot', {}).get('value', {}).get('interpretedValue')
 
-        if bqa_slot:
+        if bqa_slot == 'AnalyzingIntent':
             response = {
                 "sessionState": {
                     "dialogAction": {"type": "Close"},
@@ -22,10 +22,58 @@ def dispatch(event):
                 "messages": [
                     {
                         "contentType": "PlainText",
-                        "content": f"You chose {bqa_slot}. Please confirm by saying 'Confirm'."
+                        "content": "Which educational institute would you like to analyze? Please mention one educational institute."
                     }
                 ]
             }
+        elif bqa_slot == 'ComparingIntent':
+            response = {
+                "sessionState": {
+                    "dialogAction": {"type": "Close"},
+                    "intent": {
+                        "name": intent_name,
+                        "state": "Fulfilled"
+                    }
+                },
+                "messages": [
+                    {
+                        "contentType": "PlainText",
+                        "content": "Which educational institutes would you like to compare? Please mention two or more educational institutes."
+                    }
+                ]
+            }
+        elif bqa_slot == 'OtherIntent':
+            response = {
+                "sessionState": {
+                    "dialogAction": {"type": "Close"},
+                    "intent": {
+                        "name": intent_name,
+                        "state": "Fulfilled"
+                    }
+                },
+                "messages": [
+                    {
+                        "contentType": "PlainText",
+                        "content":  "What would you like to know?"
+                    }
+                ]
+            }
+    # else:
+    #     response = {
+    #         "sessionState": {
+    #             "dialogAction": {"type": "Close"},
+    #             "intent": {
+    #                 "name": intent_name,
+    #                 "state": "Failed"
+    #             }
+    #         },
+    #         "messages": [
+    #             {
+    #                 "contentType": "PlainText",
+    #                 "content": "I am sorry, I did not understand what you said. Please try again."
+    #             }
+    #         ]
+    #     }
         else:
             response = {
                 "sessionState": {
@@ -43,39 +91,6 @@ def dispatch(event):
                 ]
             }
 
-    elif intent_name == 'AnalyzingIntent':
-        response = {
-            "sessionState": {
-                "dialogAction": {"type": "Close"},
-                "intent": {
-                    "name": intent_name,
-                    "state": "Fulfilled"
-                }
-            },
-            "messages": [
-                {
-                    "contentType": "PlainText",
-                    "content": "Which educational institute would you like to analyze? Please mention one educational institute."
-                }
-            ]
-        }
-
-    else:
-        response = {
-            "sessionState": {
-                "dialogAction": {"type": "Close"},
-                "intent": {
-                    "name": intent_name,
-                    "state": "Failed"
-                }
-            },
-            "messages": [
-                {
-                    "contentType": "PlainText",
-                    "content": "I am sorry, I did not understand what you said. Please try again."
-                }
-            ]
-        }
 
     return response
 
