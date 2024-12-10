079a188d4b30ceb6cdf7cc3906c630d3335bee6e
AliJasim03 <122734360+AliJasim03@users.noreply.github.com>
2024-12-10 11:03:34 +0300

    modify the intenet fulfillment

--- packages/functions/src/LexBot/intentAmazonLexFulfillment.py
+++ packages/functions/src/LexBot/intentAmazonLexFulfillment.py
@@ -1,87 +1,84 @@
 import json
-import random
-from decimal import Decimal
 
 def dispatch(event):
     print('Event:', json.dumps(event, indent=2))
 
     response = None
 
-    intent_name = event.get('currentIntent', {}).get('name')
-    
+    intent_name = event.get('sessionState', {}).get('intent', {}).get('name')
+
     if intent_name == 'WelcomeIntent':
-        if event.get('currentIntent', {}).get('slots', {}).get('BQASlot'):
+        bqa_slot = event.get('sessionState', {}).get('intent', {}).get('slots', {}).get('BQASlot', {}).get('value', {}).get('interpretedValue')
+
+        if bqa_slot:
             response = {
-                'contentType': 'Y4:0',
-                'messageGroups': [{
-                    'messageGroup': {
-                        'message': {
-                            'plainTextMessage': {
-                                'value': f"You chose {event['currentIntent']['slots']['BQASlot']}. Please confirm by saying \"Confirm\"."
-                            }
-                        }
+                "sessionState": {
+                    "dialogAction": {"type": "Close"},
+                    "intent": {
+                        "name": intent_name,
+                        "state": "Fulfilled"
                     }
-                }]
+                },
+                "messages": [
+                    {
+                        "contentType": "PlainText",
+                        "content": f"You chose {bqa_slot}. Please confirm by saying 'Confirm'."
+                    }
+                ]
             }
         else:
             response = {
-                'contentType': 'N',
-                'messageGroups': [{
-                    'messageGroup': {
-                        'message': {
-                            'plainTextMessage': {
-                                'value': "Please choose an option by clicking one of the buttons."
-                            }
-                        }
+                "sessionState": {
+                    "dialogAction": {"type": "ElicitSlot", "slotToElicit": "BQASlot"},
+                    "intent": {
+                        "name": intent_name,
+                        "state": "InProgress"
                     }
-                }]
+                },
+                "messages": [
+                    {
+                        "contentType": "PlainText",
+                        "content": "Please choose an option by clicking one of the buttons."
+                    }
+                ]
             }
 
     elif intent_name == 'AnalyzingIntent':
-        if event['currentIntent']['slots']['BQASlot'] == 'Analyzing':
-            response = {
-                'contentType': 'N',
-                'messageGroups': [{
-                    'messageGroup': {
-                        'message': {
-                            'plainTextMessage': {
-                                'value': "Which educational institute would you like to analyze? Please mention one educational institute."
-                            }
-                        }
-                    }
-                }]
-            }
-        else:
-            print('Unhandled case in AnalyzingIntent')
-            response = {
-                'contentType': 'N',
-                'messageGroups': [{
-                    'messageGroup': {
-                        'message': {
-                            'plainTextMessage': {
-                                'value': "I didn't understand which option you chose. Please try again."
-                            }
-                        }
-                    }
-                }]
-            }
+        response = {
+            "sessionState": {
+                "dialogAction": {"type": "Close"},
+                "intent": {
+                    "name": intent_name,
+                    "state": "Fulfilled"
+                }
+            },
+            "messages": [
+                {
+                    "contentType": "PlainText",
+                    "content": "Which educational institute would you like to analyze? Please mention one educational institute."
+                }
+            ]
+        }
 
     else:
-        print(f'Unhandled intent: {intent_name}')
         response = {
-            'contentType': 'N',
-            'messageGroups': [{
-                'messageGroup': {
-                    'message': {
-                        'plainTextMessage': {
-                            'value': "I am sorry, I did not understand what you said. Please try again."
-                        }
-                    }
+            "sessionState": {
+                "dialogAction": {"type": "Close"},
+                "intent": {
+                    "name": intent_name,
+                    "state": "Failed"
                 }
-            }]
+            },
+            "messages": [
+                {
+                    "contentType": "PlainText",
+                    "content": "I am sorry, I did not understand what you said. Please try again."
+                }
+            ]
         }
 
     return response
+
 
 def lambda_handler(event, context):
     return dispatch(event)
