69bd6953e91dcda4daaffc19c43926e329b6f390
Hasan <hzs2003@live.com>
2024-12-29 21:20:55 +0300

    added filter

--- packages/frontend/src/pages/Filter.tsx
+++ packages/frontend/src/pages/Filter.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { LexChartSlotsContext } from "../components/RouterRoot";
 
 
 const Filter = () => {
