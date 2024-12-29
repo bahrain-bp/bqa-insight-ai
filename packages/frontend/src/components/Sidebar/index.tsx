ed745e22125dfdf5c4be373c839c03f110a3ca9d
theMycron <alyasfy12@gmail.com>
2024-12-29 18:43:58 +0300

    fix sidebar link for university page

--- packages/frontend/src/components/Sidebar/index.tsx
+++ packages/frontend/src/components/Sidebar/index.tsx
@@ -173,7 +173,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 <NavLink
                   to="/UniversityReviews"
                   className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-lightblue dark:hover:bg-meta-4 ${
-                    pathname.includes('universitiesReviewsDashboard') && 'bg-lightblue dark:bg-meta-4'
+                    pathname.includes('UniversityReviews') && 'bg-lightblue dark:bg-meta-4'
                   
                   }`}
                 >
